c537f2585f9d0bff38239fd627fb26a494cee89f
YouJunHyun <tsumego2015@gmail.com>
2023-11-08 17:53:23 +0900

    idler

--- Assets/Scripts/BehaviourTree/Details/Executers/EnemyAI.cs
+++ Assets/Scripts/BehaviourTree/Details/Executers/EnemyAI.cs
@@ -6,15 +6,23 @@ public class EnemyAI : MonoBehaviour
 {
     Actor self;
 
-	//IComposable head;
+	Selecter head;
 
 	private void Awake()
 	{
 		self = GetComponent<Actor>();
+		head = new Selecter();
+		IsInRange inRange = new IsInRange(self, GameManager.instance.player.transform, self.sight.sightRange);
+		Mover move = new Mover(self);
+		Sequencer seq = new Sequencer();
+		seq.connecteds.Add(inRange);
+		seq.connecteds.Add(move);
+		head.connecteds.Add(seq);
+
 	}
 
 	private void Update()
 	{
-		//head.Compose();
+		head.Examine();
 	}
 }
