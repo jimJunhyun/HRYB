e129df3eb32559cb05d55e73b9027470902d82b9
yuseungwon0318 <sw060318@naver.com>
2024-03-19 23:33:44 +0900

    fix

--- Assets/01_Scripts/Managers/BossHPManager.cs
+++ Assets/01_Scripts/Managers/BossHPManager.cs
@@ -5,12 +5,10 @@ using TMPro;
 
 public class BossHPManager : MonoBehaviour
 {
+	GameObject obj;
 
-
-    public GameObject makeHP(string name, LifeModule lf)
+	public GameObject makeHP(string name, LifeModule lf)
 	{
-
-		GameObject obj;
 		obj = Instantiate(GameManager.instance.pManager.bossHPBar, transform);
 		obj.GetComponentInChildren<BossHPBar>().lf = lf;
 		obj.GetComponentInChildren<TMP_Text>().text = name;
