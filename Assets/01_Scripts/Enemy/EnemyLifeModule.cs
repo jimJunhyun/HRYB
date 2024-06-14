71996c7c6bb3ec7d2b65ec8c3bc19f9db5d500f7
YouJunHyun <tsumego2015@gmail.com>
2024-06-14 15:31:13 +0900

    npc anim added with quest confirmatino

--- Assets/01_Scripts/Enemy/EnemyLifeModule.cs
+++ Assets/01_Scripts/Enemy/EnemyLifeModule.cs
@@ -206,6 +206,10 @@ public class EnemyLifeModule : LifeModule
 		}
 
 		GameManager.instance.pinven.AddExp(_expAmt);
+		if (self.show)
+		{
+			GameManager.instance.minimap.RemoveRender(self.show);
+		}
 	}
 }
 
