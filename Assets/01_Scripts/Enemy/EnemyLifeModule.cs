5e5a443caaba9ca1e8d65651f6a917253e3d346f
sungmin0427 <yoonsm0037@gmail.com>
2024-06-17 20:15:08 +0900

    성공적인 밸런싱을 하다.
    흠냐털

--- Assets/01_Scripts/Enemy/EnemyLifeModule.cs
+++ Assets/01_Scripts/Enemy/EnemyLifeModule.cs
@@ -111,34 +111,34 @@ public class EnemyLifeModule : LifeModule
 		if(yy.white.MaxValue * 0.66f > yy.white.Value && _66PercentWhite ==false)
 		{
 			_66PercentWhite = true;
-			OutValue(yy.white.MaxValue * 0.0005f);
+			OutValue(yy.white.MaxValue * 0.004f);
 		}
 		if (yy.white.MaxValue * 0.33f > yy.white.Value && _33PercentWhite == false)
 		{
 			_33PercentWhite = true;
-			OutValue(yy.white.MaxValue * 0.0005f);
+			OutValue(yy.white.MaxValue * 0.004f);
 		}
 		if(yy.white.Value <= 0 && _isDie == false)
 		{
 			_isDie = true;
-			OutValue(yy.white.MaxValue * 0.0005f);
+			OutValue(yy.white.MaxValue * 0.004f);
 		}
 
 		if (yy.black.MaxValue * 0.66f > yy.black.Value && _66PercentBlack == false)
 		{
 			_66PercentBlack = true;
-			OutValue(yy.black.MaxValue * 0.0005f);
+			OutValue(yy.black.MaxValue * 0.004f);
 		}
 		if (yy.black.MaxValue * 0.33f > yy.black.Value && _33PercentBlack == false)
 		{
 			_33PercentBlack = true;
-			OutValue(yy.black.MaxValue * 0.0005f);
+			OutValue(yy.black.MaxValue * 0.004f);
 		}
 
 		if (yy.black.Value <= 0 && _isDie ==false)
 		{
 			_isDie = true;
-			OutValue(yy.black.MaxValue * 0.0005f);
+			OutValue(yy.black.MaxValue * 0.004f);
 		}
 
 		/*
