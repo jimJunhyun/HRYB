cc080b7c03439fcce6b8ad0c40c4e8552a11c71b
yuchul06 <65890837+yuchul06@users.noreply.github.com>
2024-06-25 10:45:56 +0900

    버그 수정 (카메라 때문에 미뤄야할듯)

--- Assets/AmplifyShaderEditor/Plugins/Editor/Templates/TemplateMenuItems.cs
+++ Assets/AmplifyShaderEditor/Plugins/Editor/Templates/TemplateMenuItems.cs
@@ -61,5 +61,35 @@ namespace AmplifyShaderEditor
 		{
 			AmplifyShaderEditorWindow.CreateConfirmationTemplateShader( "5056123faa0c79b47ab6ad7e8bf059a4" );
 		}
+		[MenuItem( "Assets/Create/Amplify Shader/Universal/2D Custom Lit", false, 85 )]
+		public static void ApplyTemplateUniversal2DCustomLit()
+		{
+			AmplifyShaderEditorWindow.CreateConfirmationTemplateShader( "ece0159bad6633944bf6b818f4dd296c" );
+		}
+		[MenuItem( "Assets/Create/Amplify Shader/Universal/2D Lit", false, 85 )]
+		public static void ApplyTemplateUniversal2DLit()
+		{
+			AmplifyShaderEditorWindow.CreateConfirmationTemplateShader( "199187dac283dbe4a8cb1ea611d70c58" );
+		}
+		[MenuItem( "Assets/Create/Amplify Shader/Universal/2D Unlit", false, 85 )]
+		public static void ApplyTemplateUniversal2DUnlit()
+		{
+			AmplifyShaderEditorWindow.CreateConfirmationTemplateShader( "cf964e524c8e69742b1d21fbe2ebcc4a" );
+		}
+		[MenuItem( "Assets/Create/Amplify Shader/Universal/Decal", false, 85 )]
+		public static void ApplyTemplateUniversalDecal()
+		{
+			AmplifyShaderEditorWindow.CreateConfirmationTemplateShader( "c2a467ab6d5391a4ea692226d82ffefd" );
+		}
+		[MenuItem( "Assets/Create/Amplify Shader/Universal/Lit", false, 85 )]
+		public static void ApplyTemplateUniversalLit()
+		{
+			AmplifyShaderEditorWindow.CreateConfirmationTemplateShader( "94348b07e5e8bab40bd6c8a1e3df54cd" );
+		}
+		[MenuItem( "Assets/Create/Amplify Shader/Universal/Unlit", false, 85 )]
+		public static void ApplyTemplateUniversalUnlit()
+		{
+			AmplifyShaderEditorWindow.CreateConfirmationTemplateShader( "2992e84f91cbeb14eab234972e07ea9d" );
+		}
 	}
 }
