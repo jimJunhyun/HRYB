371a2fd0840ab475de5aff20545d0640d4d29c3f
YouJunHyun <tsumego2015@gmail.com>
2024-06-25 13:01:10 +0900

    minigametutorials

--- Assets/01_Scripts/Minigames/Cuts/BarSwiftMove.cs
+++ Assets/01_Scripts/Minigames/Cuts/BarSwiftMove.cs
@@ -12,23 +12,32 @@ public class BarSwiftMove : MonoBehaviour
 	Scrollbar scr;
 
 	float accTime = 0;
+	bool started;
 
 	private void Awake()
 	{
 		scr = GetComponent<Scrollbar>();
 		accTime = 0;
 		ChangeSpeed();
+		started = false;
 	}
 
 	private void Update()
 	{
-		accTime += Time.unscaledDeltaTime * barMoveSpeed;
-		scr.value = Mathf.Lerp(0, 1, (Mathf.Sin(accTime) * 0.5f + 0.5f));
-
+		if (started)
+		{
+			scr.value = Mathf.Lerp(0, 1, (Mathf.Sin(accTime) * 0.5f + 0.5f));
+			accTime += Time.unscaledDeltaTime * barMoveSpeed;
+		}
 	}
 
 	public void ChangeSpeed()
 	{
 		barMoveSpeed = Random.Range(barRandomJitter.x, barRandomJitter.y);
+	}
+
+	public void DoStart()
+	{
+		started = true;
 	}
 }
