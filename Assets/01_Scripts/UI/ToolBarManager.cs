24e8d67f8f28b84aa52ccc857e9594f425b40d23
yuchul06 <65890837+yuchul06@users.noreply.github.com>
2024-07-02 11:14:02 +0900

    머지용

--- Assets/01_Scripts/UI/ToolBarManager.cs
+++ Assets/01_Scripts/UI/ToolBarManager.cs
@@ -31,7 +31,7 @@ public class ToolBarManager : MonoBehaviour
 	{
 		List<Transform> childs = new List<Transform>();
 		ToolState[] arr = (ToolState[])System.Enum.GetValues(typeof(ToolState));
-		for (int i = 1; i < transform.childCount; i++) // Index 0 = ToolBar
+		for (int i = 0; i < transform.childCount-1; i++) // Last Index = ToolBar
 		{
 			childs.Add(transform.GetChild(i));
 		}
